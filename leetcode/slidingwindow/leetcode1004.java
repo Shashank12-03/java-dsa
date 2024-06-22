74d73c12453823ca5bc5cf118c7e4a7c0b6660e8
Shashank12-03 <120593640+Shashank12-03@users.noreply.github.com>
2024-06-22 16:12:26 +0530

    max no. of ones

--- leetcode/slidingwindow/leetcode1004.java
+++ leetcode/slidingwindow/leetcode1004.java
@@ -3,23 +3,31 @@ package slidingwindow;
 
 public class leetcode1004 {
     public static void main(String[] args) {
-        System.out.println(longestOnes(new int[]{1,1,1,0,0,0,1,1,1,1,0}, 2));
+        // System.out.println(longestOnes(new int[]{1,1,1,0,0,0,1,1,1,1,0}, 2));
         System.out.println(longestOnes(new int[]{0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1}, 3));
     }
     public static int longestOnes(int[] nums, int k) {
-        int start=0;
-        int end=0;
-        int max_length=0;
-        while (end<nums.length) {
-            if(nums[end]==1){
-                end++;
+        int st = 0;
+        int en = 0;
+        int maxNoOfOnes = 0;
+        int noOfZeros = 0;
+
+        while (en < nums.length) {
+            if (nums[en] == 0) {
+                noOfZeros++;
             }
-            if(nums[end]==0 && k>0){
-                end++;
-                k--;
+
+            while (noOfZeros > k) {
+                if (nums[st] == 0) {
+                    noOfZeros--;
+                }
+                st++;
             }
-            start++;
+
+            maxNoOfOnes = Math.max(maxNoOfOnes, en - st + 1);
+            en++;
         }
-        return max_length;
+        
+        return maxNoOfOnes;
     }
 }
